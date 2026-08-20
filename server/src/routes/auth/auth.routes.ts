cc36198915386b8954720fc0bf03d6374c37d35a
Aishwarya Tekade <157888799+AishwaryaT123@users.noreply.github.com>
2026-08-21 00:46:21 +0530

    Update auth.routes.ts

--- server/src/routes/auth/auth.routes.ts
+++ server/src/routes/auth/auth.routes.ts
@@ -1,8 +1,18 @@
 import { Router } from "express";
-import { register } from "../../controllers/auth/auth.controller";
+import { register, login, } from "../../controllers/auth/auth.controller";
+import { authenticate } from "../../middleware/auth.middleware";
 
 const router = Router();
 
 router.post("/register", register);
+router.post("/login", login);
+
+router.get("/me", authenticate, (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Authenticated user",
+    user: req.user,
+  });
+});
 
 export default router;
