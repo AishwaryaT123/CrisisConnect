b9b75169535604415bac80912ba39b394cc8a21d
Aishwarya Tekade <157888799+AishwaryaT123@users.noreply.github.com>
2026-08-21 00:44:40 +0530

    Add login

--- server/src/services/auth/auth.service.ts
+++ server/src/services/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { prisma } from "../../config/database";
 import { UserRole } from "../../generated/prisma/client";
 import { generateToken } from "../../utils/jwt";
 
+//Register
 interface RegisterInput {
   name: string;
   email: string;
@@ -33,6 +34,49 @@ export const registerUser = async (data: RegisterInput) => {
       role: data.role ?? UserRole.CITIZEN,
     },
   });
+
+  const token = generateToken({
+    userId: user.id,
+    role: user.role,
+  });
+
+  return {
+    token,
+    user: {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      phone: user.phone,
+      role: user.role,
+    },
+  };
+};
+
+//Login
+interface LoginInput {
+  email: string;
+  password: string;
+}
+
+export const loginUser = async (data: LoginInput) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      email: data.email,
+    },
+  });
+
+  if (!user) {
+    throw new Error("Invalid email or password");
+  }
+
+  const passwordMatch = await bcrypt.compare(
+    data.password,
+    user.password
+  );
+
+  if (!passwordMatch) {
+    throw new Error("Invalid email or password");
+  }
 
   const token = generateToken({
     userId: user.id,
